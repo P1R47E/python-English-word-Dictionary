c1e3a578d617cd1de5ac27720094a84e14b32265
pirate776 <kingzaljaguar@gmail.com>
2024-02-27 09:24:09 +0300

    libraries-imported

---
+++ Dictionary.py
@@ -0,0 +1,5 @@
+import difflib #difflib is a library here
+import json
+from difflib import get_close_matches #optional as SequenceMatcher(Read it later)
+
+"""get_close_matches module returns the ratio of the desired word with compared in the possibilities"""
