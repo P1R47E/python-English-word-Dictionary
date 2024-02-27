d40b280c364f5b2501ec7b4c47eb89c43e7e1b1a
pirate776 <kingzaljaguar@gmail.com>
2024-02-27 09:26:55 +0300

    get-close-match

--- Dictionary.py
+++ Dictionary.py
@@ -6,3 +6,11 @@ from difflib import get_close_matches #optional as SequenceMatcher(Read it later
 data = json.load(open("data.json"))
 def translator(words):
     words = words.lower()
+    if words in data:
+        
+        return data[words]
+    elif len(get_close_matches(words,data.keys())) > 0:#means if the returned list is not empty
+        suggestion = input("Did you mean %s instead? Enter Y if yes or N if no: " %get_close_matches(words,data.keys())[0])
+        suggestion = suggestion.upper()
+        if suggestion == "Y":
+            return data[get_close_matches(words,data.keys())[0]]
