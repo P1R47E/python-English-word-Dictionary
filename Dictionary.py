1f77b69176365c5ec640c21ccb52bf012157d429
pirate776 <kingzaljaguar@gmail.com>
2024-02-27 09:25:11 +0300

    json file import

--- Dictionary.py
+++ Dictionary.py
@@ -3,3 +3,6 @@ import json
 from difflib import get_close_matches #optional as SequenceMatcher(Read it later)
 
 """get_close_matches module returns the ratio of the desired word with compared in the possibilities"""
+data = json.load(open("data.json"))
+def translator(words):
+    words = words.lower()
